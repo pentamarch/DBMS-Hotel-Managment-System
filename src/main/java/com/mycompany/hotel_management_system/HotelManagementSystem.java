f90a283e784d4ecc78f82d22615af468275d3a91
aditi <pentamarch2000@gmail.com>
2020-11-07 15:24:42 +0530

    first commit

---
+++ src/main/java/com/mycompany/hotel_management_system/HotelManagementSystem.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.hotel_management_system;
+
+/**
+ *
+ * @author aditi
+ */
+public class HotelManagementSystem {
+    
+}
